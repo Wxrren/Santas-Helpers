aae7dc3eab8825b67c1d70d7a31fc66b2b7eaab8
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-07-16 21:55:25 +0000

    ADD: Imports for Flask-WTF or WTForms to handle form validation.

--- santashelpers/routes.py
+++ santashelpers/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template
 from santashelpers import app, db
+from sqlalchemy.orm import relationship
+from wtforms import Form, StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 @app.route("/")
