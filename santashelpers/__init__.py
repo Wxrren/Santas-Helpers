989cfa604e10396167a4260bd67e1f83216714ab
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-07-16 21:54:31 +0000

    ADD: Flask Login

--- santashelpers/__init__.py
+++ santashelpers/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 if os.path.exists("env.py"):
     import env  # noqa
 
@@ -10,5 +11,8 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from santashelpers import routes  # noqa
