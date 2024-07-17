3080fffa2a6c4e25854dce2c1638d0ecbaaca8e2
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-07-17 23:11:43 +0000

    ADD: Background image styles

--- santashelpers/routes.py
+++ santashelpers/routes.py
@@ -1,10 +1,19 @@
 from flask import render_template
 from santashelpers import app, db
 from sqlalchemy.orm import relationship
-from wtforms import Form, StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
+
 
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("landing_page.html")
+
+
+@app.route("/register_user")
+def register_user():
+    return render_template("register.html")
+
+
+@app.route("/sign_in")
+def sign_in():
+    return render_template("sign_in.html")
