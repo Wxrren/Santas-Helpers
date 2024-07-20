bd6cc6b03c9607c6d5d5c99710e0f8710084c205
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-07-20 16:21:12 +0000

    UPDATE: set user model added security

--- santashelpers/models.py
+++ santashelpers/models.py
@@ -1,27 +1,18 @@
 from santashelpers import db
-from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
 
-class List(db.Model):
-    # schema for the user relationship model
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ForeignKey('users_id'))
-    items = Column(db.String(25), nullable=False)
-    user = relationship('User', back_populates='lists')
-
-
-class Christmas_List(db.Model):
-    # schema for the Category model
-    id = db.Column(db.Integer, primary_key=True)
-    christmas_list_name = db.Column(db.String(25), unique=True, nullable=False)
-    christmas_list_date = db.Column(db.Date, nullable=False)
-    christmas_list_description = db.Column(db.Text, nullable=False)
-    christmas_list_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    fullname = db.Column(db.String(25), nullable=False)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
-       # __repr__ to represent itself in the form of a string
-        return "#{0} - Task: {1} | Urgent: {2}".format(
-            self.id, self.task_name, self.is_urgent
-        )
+        return f"<User {self.id} - {self.username}>"
+
+ 
