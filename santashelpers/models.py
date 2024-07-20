fb9236c23f55ea14ebe26b5f2191566c11e4aaf4
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-07-20 21:08:10 +0000

    FIX: Issue with forms not posting to db - new model table added

--- santashelpers/models.py
+++ santashelpers/models.py
@@ -5,24 +5,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    fullname = db.Column(db.String(25), nullable=False)
-    username = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.Column(db.String(300), nullable=False)
-
-    def __repr__(self):
-        return f"<User {self.id} - {self.username}>"
-
-
 class Activeuser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True, nullable=False)
     fullname = db.Column(db.String(25), nullable=False)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.Column(db.String(300), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return f"<User {self.id} - {self.username}>"
- 
