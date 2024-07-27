966ebfcf240ffa1191861df1756b34e3daa6cafd
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-07-27 17:02:11 +0000

    ADD: steps for deploting to heroku

--- santashelpers/__init__.py
+++ santashelpers/__init__.py
@@ -1,18 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
 if os.path.exists("env.py"):
     import env  # noqa
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
 
 db = SQLAlchemy(app)
 
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 
-from santashelpers import routes  # noqa
+from santashelpers import routes  #noqa
